f5e2c690deee532c9757340e4e8e374fda79a6e4
Robson Joao Padilha da Luz <robson.luz@bomjesus.br>
2016-09-01 12:58:36 -0300

    projeto inicial

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = require('./lib/base-app');
+
+
+/*
+"use strict";
+
+const http = require("http");
+const URL = require("url");
+const configClient = require("cloud-config-client");
+
+
+
+function load(options) {
+    console.log('modulo carregado');
+}
+
+module.exports = { load }
+
+
+*/
